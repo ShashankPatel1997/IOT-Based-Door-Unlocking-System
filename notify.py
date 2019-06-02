4f97696e4f0b33d05d37b6b14613fbac6c648f0d
ShashankPatel1997 <32807832+ShashankPatel1997@users.noreply.github.com>
2019-06-02 20:28:48 +0530

    Update notify.py

--- notify.py
+++ notify.py
@@ -1,25 +1,24 @@
 import pyrebase
 from pusher_push_notifications import PushNotifications
-
-config ={
-    "apiKey": "AIzaSyB36ZzOR77egxe7iUWkR9DC6-jZDEplNqI",
-    "authDomain": "smartdoorfinal-a7750.firebaseapp.com",
-    "databaseURL": "https://smartdoorfinal-a7750.firebaseio.com",
-    "projectId": "smartdoorfinal-a7750",
-    "storageBucket": "smartdoorfinal-a7750.appspot.com",
-    "messagingSenderId": "1008216095743"
+config ={   # copy config details of firebase here
+    "apiKey": "",
+    "authDomain": "",
+    "databaseURL": "",
+    "projectId": "",
+    "storageBucket": "",
+    "messagingSenderId": ""
   };
 
 firebase = pyrebase.initialize_app(config)
 db=firebase.database()
-pn_client = PushNotifications(
-    instance_id='7079dca0-843c-49fd-9479-defbc306ecd2',
-    secret_key='4B882BD902AEE83ADFC91935E01F3CD04D6EE026FE63BEACDC1CD879B4AE42B9',
+pn_client = PushNotifications(   # Copy id and key from pusher account
+    instance_id='',
+    secret_key='',
 )
 
 def stream_handler(message):
     print(message)
-    if(message['data'] is 1):
+    if(message['data'] is 1):  # data is a child node in firebase database
         response = pn_client.publish_to_interests( interests=['hello'],publish_body={ 'apns': {'aps': {'alert': 'Hello!'}},'fcm': {'notification': {'title': 'Alert!!','sound':'mySound','body': 'Someone Pressed the Doorbell !'}}})
         print(response['publishId'])
     
